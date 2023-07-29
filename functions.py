8a493e61fd6abc0614c1568db33abf7e354ef936
CamiloDev <zuluagajuanca@outlook.es>
2023-07-28 19:59:34 -0500

    added some comments

--- functions.py
+++ functions.py
@@ -1,10 +1,11 @@
+# Required imports
 from tkinter import *
 from PIL import Image
 from tkinter import filedialog
 from PIL import Image
 import img2pdf
 
-
+# Convertion options
 menu_options = {
     1: 'From [JPG -> PNG]',
     2: 'From [PNG -> JPG]',
@@ -21,6 +22,7 @@ def print_menu():
     for key in menu_options.keys():
         print (key, '--', menu_options[key] )
 
+# Select file menu display
 def converter_menu(answer):
     global root
     
@@ -45,11 +47,14 @@ def converter_menu(answer):
     root.mainloop()
 
 
+# Main function
 def open_dialog_convertion(answer):
     global im1
     global filename
     
-    if answer == "1":
+    # Executing the user´s input
+    # JPG TO PNG
+    if answer == "1": 
         print("\n··· [JPG -> PNG] Selected ···")
         filename = filedialog.askopenfilename(initialdir="/", title="Select A File", filetypes=(("JPG files", "*.jpg"),("All files", "*.*")))
         if filename.endswith(".jpg"):
@@ -61,7 +66,8 @@ def open_dialog_convertion(answer):
             root.destroy()
         else:
             print("Error - Something went wrong :(")
-            
+    
+    # PNG to JPG        
     elif answer == "2":
         print("··· [PNG -> JPG] Selected ···")
         filename = filedialog.askopenfilename(initialdir="/", title="Select A File", filetypes=(("PNG files", "*.png"),("All files", "*.*")))
@@ -74,7 +80,8 @@ def open_dialog_convertion(answer):
             root.destroy()
         else:
             print("Error - Something went wrong :(")
-        
+    
+    # PNG/JPG to ICO  
     elif answer == "3":
         print("··· [PNG/JPG -> ICO] Selected ···")
         filename = filedialog.askopenfilename(initialdir="/", title="Select A File", filetypes=(("PNG files", "*.png"),("JPG files", "*.jpg")))
@@ -87,7 +94,8 @@ def open_dialog_convertion(answer):
             root.destroy()
         else:
             print("Error - Something went wrong :(")
-            
+    
+    # PDF convertion using img2pdf
     elif answer == "4":
         print("··· [PNG/JPG -> PDF] Selected ···")
         print("\n - You can select multiple images -")
@@ -114,7 +122,8 @@ def open_dialog_convertion(answer):
                 print("Error - No file selected for saving.")
         else:
             print("Error - No files selected.")
-            
+    
+    # PNG/JPG to WEBP
     elif answer == "5":
         print("··· [PNG/JPG -> WEBP] Selected ···")
         filename = filedialog.askopenfilename(initialdir="/", title="Select A File", filetypes=(("PNG files", "*.png"),("JPG files", "*.jpg")))
@@ -128,7 +137,7 @@ def open_dialog_convertion(answer):
         else:
             print("Error - Something went wrong :(")
             
-            
+    # WEBP to PNG
     elif answer == "6":
         print("··· [WEBP -> PNG] Selected ···")
         filename = filedialog.askopenfilename(initialdir="/", title="Select A File", filetypes=(("WEBP files", "*.webp"),("All files", "*.*")))
@@ -141,7 +150,8 @@ def open_dialog_convertion(answer):
             root.destroy()
         else:
             print("Error - Something went wrong :(")
-            
+    
+    # WEBP to JPG
     elif answer == "7":
         print("··· [WEBP -> JPG] Selected ···")
         filename = filedialog.askopenfilename(initialdir="/", title="Select A File", filetypes=(("WEBP files", "*.webp"),("All files", "*.*")))
@@ -154,7 +164,8 @@ def open_dialog_convertion(answer):
             root.destroy()
         else:
             print("Error - Something went wrong :(")
-            
+    
+    # ICO to JPG
     elif answer == "8":
         print("··· [ICO -> JPG] Selected ···")
         filename = filedialog.askopenfilename(initialdir="/", title="Select A File", filetypes=(("ICO files", "*.ico"),("All files", "*.*")))
@@ -167,6 +178,8 @@ def open_dialog_convertion(answer):
             root.destroy()
         else:
             print("Error - Something went wrong :(")
+    
+    # ICO to PNG
     elif answer == "9":
         print("··· [ICO -> PNG] Selected ···")
         filename = filedialog.askopenfilename(initialdir="/", title="Select A File", filetypes=(("ICO files", "*.ico"),("All files", "*.*")))
